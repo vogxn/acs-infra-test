bdbf74723c2f41934cd2fa8ed288c787971d142f
Prasanna Santhanam <Prasanna.Santhanam@citrix.com>
2012-09-20 18:35:46 +0530

    Changes to mysql-server packags

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,5 +1,19 @@
 class mysql {
-  package { 'mysql':
-    ensure => present,
+  package { "mysql-server":
+    name     => $operatingsystem? {
+      centos => ["mysql-server", "mysql-connector-java", "mysql"],
+      redhat => ["mysql-server", "mysql-connector-java", "mysql"],
+      debian => ["mysql-server", "mysql-client"],
+      ubuntu => ["mysql-server", "mysql-client"],
+    },
+    ensure   => installed,
+  }
+
+  service { "mysqld":
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
+    require    => Package[ "mysql-server" ],
   }
 }
