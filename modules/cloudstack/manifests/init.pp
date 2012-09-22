0a31534c3c4b4f48bd46a3b06fff60fa12ced331
Prasanna Santhanam <Prasanna.Santhanam@citrix.com>
2012-09-22 17:31:27 +0530

    Agent class and refactoring

--- modules/cloudstack/manifests/init.pp
+++ modules/cloudstack/manifests/init.pp
@@ -1,19 +1,12 @@
-#Apache Cloudstack - Module for the Management Server
-
-class cloudstack::no_selinux {
-  file { "/etc/selinux/config":
-    source  => "puppet://puppet/cloudstack/config",
-  }
-  exec { "/usr/sbin/setenforce 0":
-    onlyif => "/usr/sbin/getenforce | grep Enforcing",
-  }
-}
+#Apache Cloudstack - Module for the Management Server and Agent
 
 class cloudstack {
   case $operatingsystem  {
     centos: { include cloudstack::no_selinux }
     redhat: { include cloudstack::no_selinux }
   }
+  include cloudstack::repo
+  include cloudstack::ports
 
   #TODO: Repo replace from nodes.pp
   $yumrepo = "puppet://puppet/cloudstack/yumrepo"
@@ -55,36 +48,8 @@ class cloudstack {
     require => Package["nfs-utils"],
   }
 
-  #Fetch latest from repo
-  #exec { "/usr/bin/wget -m $repo -nH --cut-dirs=2 -np -L -P cloudstack --reject=index.html*":
-  #  cwd => "/tmp",
-  #  creates => "/tmp/cloudstack",
-  #}
-
-  #exec { "/bin/mv master/build* master/build":
-  #  cwd     => "/tmp/cloudstack/",
-  #  creates => "/tmp/cloudstack/master/build",
-  #}
-
   case $operatingsystem {
     centos,redhat : {
-      #TODO: Latest cloudstack build copied to puppetmaster
-      file { "/tmp/cloudstack" :
-        source  => "puppet://puppet/cloudstack/yumrepo",
-        recurse => true,
-        owner   => "root",
-        mode    => 0644,
-        group   => "root",
-        ensure  => directory,
-        path    => "/tmp/cloudstack",
-      }
-      yumrepo { "cstemp":
-        baseurl  => "file:///tmp/cloudstack",
-        enabled  => 1,
-        gpgcheck => 0,
-        name     => "cstemp",
-	require => File["/tmp/cloudstack"],
-      }
       $packagelist =  [ "cloud-server", "cloud-client"]
       package { $packagelist:
          ensure  => installed,
@@ -95,16 +60,6 @@ class cloudstack {
       }
     }
     ubuntu, debian: {
-      file { "/etc/apt/sources.list.d/cloudstack.list":
-        ensure  => present,
-        content => "deb ${aptrepo} ${lsbdistcodename} 4.0",
-      }
-      exec { "wget -O - ${aptkey} | apt-key add -": 
-        path => ["/usr/bin", "/bin"],
-      }
-      exec { "apt-get update":
-        path => ["/usr/bin", "/bin"],
-      }
       $packagelist =  [ "cloud-server", "cloud-client"]
       package { $packagelist:
          ensure  => latest,
@@ -125,10 +80,137 @@ class cloudstack {
   file { "/etc/hosts":
     content => template("cloudstack/hosts"),
   }
+}
 
+class cloudstack::agent {
+  case $operatingsystem  {
+    centos: { include cloudstack::no_selinux }
+    redhat: { include cloudstack::no_selinux }
+  }
+  include cloudstack::repo
+  include cloudstack::ports
+
+  case $operatingsystem {
+    centos,redhat : {
+      $packagelist =  [ "cloud-agent" ]
+      package { $packagelist:
+         ensure  => installed,
+         require => Yumrepo["cstemp"],
+      }
+      file { "/etc/yum.repos.d/cstemp.repo":
+        ensure => absent,
+      }
+    }
+    ubuntu, debian: {
+      $packagelist =  [ "cloud-agent" ]
+      package { $packagelist:
+         ensure  => latest,
+         require => File["/etc/apt/sources.list.d/cloudstack.list"],
+      }
+    }
+    fedora : {
+    }
+  }
+
+  exec { "cloud-setup-agent":
+    creates  => "/var/log/cloud/setupAgent.log",
+    requires => [ Package[cloud-agent],
+    Package[NetworkManager],
+    File["/etc/sudoers"],
+    File["/etc/cloud/agent/agent.properties"],
+    File["/etc/sysconfig/network-scripts/ifcfg-eth0"],
+    File["/etc/hosts"],
+    File["/etc/sysconfig/network"],
+    File["/etc/resolv.conf"],
+    Service["network"], ]
+  }
+
+  file { "/etc/cloud/agent/agent.properties":
+    ensure   => present,
+    requires => Package[cloud-agent],
+    content  => template("cloudstack/agent.properties")
+  }
+
+  file { "/etc/sysconfig/network-scripts/ifcfg-eth0":
+    content => template("cloudstack/ifcfg-eth0"),
+  }
+
+  service { network:
+    ensure    => running,
+    enabed    => true,
+    hasstatus => true, 
+    requires  => [ Package[NetworkManager], File["/etc/sysconfig/network-scripts/ifcfg-eth0"], ]
+  }
+
+  package { NetworkManager:
+    ensure => absent,
+  }
+
+  file { "/etc/sysconfig/network":
+    content => template("cloudstack/network"),
+  }
+
+  file { "/etc/resolv.conf":
+    content => template("cloudstack/resolv.conf"),
+  }
+}
+
+class cloudstack::no_selinux {
+  file { "/etc/selinux/config":
+    source  => "puppet://puppet/cloudstack/config",
+  }
+  exec { "/usr/sbin/setenforce 0":
+    onlyif => "/usr/sbin/getenforce | grep Enforcing",
+  }
+}
+
+class cloudstack::repo {
+  case $operatingsystem {
+    centos,redhat : {
+      file { "/tmp/cloudstack" :
+        source  => "puppet://puppet/cloudstack/yumrepo",
+        recurse => true,
+        owner   => "root",
+        mode    => 0644,
+        group   => "root",
+        ensure  => directory,
+        path    => "/tmp/cloudstack",
+      }
+      yumrepo { "cstemp":
+        baseurl  => "file:///tmp/cloudstack",
+        enabled  => 1,
+        gpgcheck => 0,
+        name     => "cstemp",
+      	require => File["/tmp/cloudstack"],
+      }
+    }
+    ubuntu, debian: {
+      file { "/etc/apt/sources.list.d/cloudstack.list":
+        ensure  => present,
+        content => "deb ${aptrepo} ${lsbdistcodename} 4.0",
+      }
+      exec { "wget -O - ${aptkey} | apt-key add -": 
+        path => ["/usr/bin", "/bin"],
+      }
+      exec { "apt-get update":
+        path => ["/usr/bin", "/bin"],
+      }
+    }
+    fedora : {
+    }
+  }
+}
+
+class cloudstack::ports {
   iptables { "apiport":
     proto => "tcp",
-    dport => [8096, 8080],
+    dport => [8096, 8080, 9090],
+    jump  => "ACCEPT",
+  }
+
+  iptables { "mysqlport":
+    proto => "tcp",
+    dport => 3306,
     jump  => "ACCEPT",
   }
 
