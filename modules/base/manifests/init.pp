17f2c039cdb504cda6b5ff3ae6c6bd6263631fe8
Prasanna Santhanam <Prasanna.Santhanam@citrix.com>
2012-09-25 11:28:04 +0530

    New dhcp nodes. Shorten dynamic range to 50-80. 40.x used for helper vMs

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,6 +1,10 @@
 class base {
-        package { screen: ensure => latest }
-        package { vim-enhanced: ensure => latest}
+  package {
+    screen: ensure => latest 
+  }
+  package {
+    vim-enhanced: ensure => latest
+  }
 
   firewall { '000 allow packets with valid state':
     state  => ['RELATED', 'ESTABLISHED'],
@@ -11,22 +15,23 @@ class base {
     proto  => 'icmp',
     action => 'accept',
   }
+  
   firewall { '002 allow all to lo interface':
-    iniface       => 'lo',
-    action        => 'accept',
+    iniface => 'lo',
+    action  => 'accept',
   }
+
   firewall { '100 allow ssh':
-    proto       => 'tcp',
-    dport       => '22',
-    action        => 'accept',
+    proto  => 'tcp',
+    dport  => '22',
+    action => 'accept',
   }
+
   firewall { '999 drop everything else':
-    action        => 'reject',
+    action => 'reject',
   }
 
   resources { 'firewall':
     purge => false,
   }
-
-
 }
