efc7de394bf9048d10e61f1b6181d92c941c44d8
Prasanna Santhanam <Prasanna.Santhanam@citrix.com>
2012-09-26 14:29:28 +0530

    indent

--- modules/jenkins/manifests/builder.pp
+++ modules/jenkins/manifests/builder.pp
@@ -1,23 +1,21 @@
 class jenkins::builder {
 
-  package { ant: ensure => present }
-  package { log4j: ensure => present }
-  package { cglib: ensure => present }
-  package { genisoimage: ensure => present } 
-  package { checkstyle: ensure => present } 
+  package { ant: ensure          => present }
+  package { log4j: ensure        => present }
+  package { cglib: ensure        => present }
+  package { genisoimage: ensure  => present }
+  package { checkstyle: ensure   => present }
   package { mysql-server: ensure => present}
-  package { gcc: ensure => present }
-  package { glibc-devel: ensure => present }
-  package { MySQL-python: ensure => present } 
+  package { gcc: ensure          => present }
+  package { glibc-devel: ensure  => present }
+  package { MySQL-python: ensure => present }
   service { mysqld:
     name => $operatingsystem ? {
       default => "mysqld",
-       },
-      ensure => running,
-      enable => true,
-      hasstatus => true,
-      require => Package[mysql-server],
+    },
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    require => Package[mysql-server],
   }
-
-
 }
