8eb1272d97c8b19657a36f81a0c1b4be11b504a1
Prasanna Santhanam <Prasanna.Santhanam@citrix.com>
2012-09-26 14:14:37 +0530

    Need createrepo

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -1,28 +1,27 @@
 class jenkins { 
 
   yumrepo { jenkins: 
-    baseurl => "http://pkg.jenkins-ci.org/redhat",
-    enabled => 1,
-    name => jenkins,
+    baseurl  => "http://pkg.jenkins-ci.org/redhat",
+    enabled  => 1,
+    name     => jenkins,
     gpgcheck => 0,
   }
 
-  package { jenkins: ensure => present }
-  package { 'java-1.7.0-openjdk': ensure => present}
+  package { jenkins: ensure                    => present }
+  package { 'java-1.7.0-openjdk': ensure       => present}
   package { 'java-1.7.0-openjdk-devel': ensure => present}
-  #package { dejavu-lgc-sans-fonts: ensure => present}
-  #package { dejavu-lgc-sans-mono-fonts: ensure => present}
-  package { tomcat6: ensure => present}
-  package { git: ensure => latest}
-  #package { publican: ensure => latest}
-  package { mysql-connector-java: ensure => present}
-  package { maven: ensure => present} 
-  package { wget: ensure => present} 
+  package { 'createrepo' : ensure              => present}
+  package { tomcat6: ensure                    => present}
+  package { git: ensure                        => latest}
+  package { mysql-connector-java: ensure       => present}
+  package { maven: ensure                      => present}
+  package { wget: ensure                       => present}
+
   service { jenkins: 
-    require => Package[jenkins],
-    enable => true,
+    require   => Package[jenkins],
+    enable    => true,
     hasstatus => true,
-    ensure => true,
+    ensure    => true,
   }
 
   iptables { 'http80':
