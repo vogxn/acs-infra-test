994d6b4243267db6a026025c69abdfbd62235b13
Prasanna Santhanam <Prasanna.Santhanam@citrix.com>
2012-10-17 02:20:29 +0530

    firewal changes

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -11,32 +11,31 @@ class base {
 
   service { "iptables":
     ensure  => running,
-    require => Package["iptables"],
   }
 
   firewall { '000 allow packets with valid state':
     state  => ['RELATED', 'ESTABLISHED'],
-    action => 'accept',
+    action => accept,
   }
 
   firewall { '001 allow icmp':
     proto  => 'icmp',
-    action => 'accept',
+    action => accept,
   }
   
   firewall { '002 allow all to lo interface':
     iniface => 'lo',
-    action  => 'accept',
+    action  => accept,
   }
 
   firewall { '100 allow ssh':
     proto  => 'tcp',
     dport  => '22',
-    action => 'accept',
+    action => accept,
   }
 
   firewall { '999 drop everything else':
-    action => 'reject',
+    action => reject,
   }
 
   #  resources { 'firewall':
