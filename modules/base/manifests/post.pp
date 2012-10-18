7474c7680ed1da18a3d1757e6274b14b4b36d2c6
Prasanna Santhanam <Prasanna.Santhanam@citrix.com>
2012-10-18 20:49:15 +0530

    not rejects for now

--- modules/base/manifests/post.pp
+++ modules/base/manifests/post.pp
@@ -1,7 +1,7 @@
 class base::post {
-  firewall { '999 drop everything else':
-    proto  => all,
-    action => reject,
-    before => undef,
-  }
+  #  firewall { '999 drop everything else':
+  #  proto  => all,
+  #  action => reject,
+  #  before => undef,
+  #}
 }
