8460ac4477c4e19a30ce7e2018a70dfc1aad6963
Prasanna Santhanam <Prasanna.Santhanam@citrix.com>
2012-10-18 17:21:09 +0530

    site .pp changes

--- manifests/site.pp
+++ manifests/site.pp
@@ -35,8 +35,8 @@ exec { 'persist-firewall':
 # first puppet run.
 Firewall {
   notify  => Exec['persist-firewall'],
-  require => Class['base::pre'],
-  before  => Class['base::post'],
+  require => Class['fw_base::pre'],
+  before  => Class['fw_base::post'],
 }
 Firewallchain {
   notify  => Exec['persist-firewall'],
